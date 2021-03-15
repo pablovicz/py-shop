261ce91569e65f9886a8f382861382f45751076e
Pablo <pablowoina2205@gmail.com>
2021-03-15 14:26:43 -0300

    frontend initial commit

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Navbar, Nav, Container, Row, Form, FormControl, Button } from 'react-bootstrap'
+
+function Header() {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="#home">PyShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">
+              <i className="fas fa-shopping-cart"></i>
+              Cart
+              </Nav.Link>
+            <Nav.Link href="#link">
+              <i className="fas fa-user"></i>
+              Login
+            </Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
