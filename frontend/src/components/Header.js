0d34c9ca46f010d2c0918ff2c5414a627a155c7a
pablo.wramos <pablo.wramos@telefonica.com>
2021-03-15 16:48:50 -0300

    HomeScreen, ProductScreen.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import { Navbar, Nav, Container, Row, Form, FormControl, Button } from 'react-bootstrap'
+import reactDom from 'react-dom'
+import {LinkContainer} from 'react-router-bootstrap'
 
 function Header() {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="#home">PyShop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>PyShop</Navbar.Brand>
+          </LinkContainer>
+          
+          
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">
-              <i className="fas fa-shopping-cart"></i>
-              Cart
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <i className="fas fa-shopping-cart"></i>
+                Cart
+                </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/link">
+              <Nav.Link >
+                <i className="fas fa-user"></i>
+                Login
               </Nav.Link>
-            <Nav.Link href="#link">
-              <i className="fas fa-user"></i>
-              Login
-            </Nav.Link>
+            </LinkContainer>
           </Nav>
           </Navbar.Collapse>
         </Container>
